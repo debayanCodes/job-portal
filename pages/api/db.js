a359be56933dc19ebe08e69e22c41f9d7415671f
debayanCodes <downloadingmedia@gmail.com>
2024-10-06 22:19:13 +0530

    update

--- pages/api/db.js
+++ pages/api/db.js
@@ -1,7 +1,7 @@
 import mysql from 'mysql2/promise'
 
 const pool = mysql.createPool({
-  hhost: process.env.DB_HOST,         // Use environment variable for host
+  host: process.env.DB_HOST,         // Use environment variable for host
   user: process.env.DB_USER,         // Use environment variable for user
   password: process.env.DB_PASSWORD,  // Use environment variable for password
   database: process.env.DB_NAME,      // Use environment variable for database name
